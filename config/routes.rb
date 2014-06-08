e6b188f5e42d2664fb646c96f1185719e1f598b0
wTendera <wiktortendera@gmail.com>
2014-06-08 12:52:42 +0200

    Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Library::Application.routes.draw do
-  get "users/new"
+  resources :users
   root 'static_pages#home'
   match '/home', to: 'static_pages#home', via: 'get'
   match '/login', to: 'static_pages#login', via: 'get'
